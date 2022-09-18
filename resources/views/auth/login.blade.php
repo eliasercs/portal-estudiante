6331653ee0233d8bb99409c45d71e01ee6a40c11
Alvaro Valenzuela <108038544+MelonCaja@users.noreply.github.com>
2022-09-17 21:27:38 -0300

    Add files via upload

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,31 +7,77 @@
 <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 
 rounded-lg shadow-lg">
 
-  <h1 class="text-3xl text-center font-bold">Login</h1>
+<h5 class="fw-bold mb-4 pb-4" style="letter-spacing: 1.5px;">Iniciar sesión</h5>
 
   <form class="mt-4" method="POST" action="">
     @csrf
 
-    <input type="email" class="border border-gray-200 rounded-md bg-gray-200 w-full
-    text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Email"
-    id="email" name="email">
+<section class="vh-100" style="background-color: grey;">
+  <div class="container py-5 h-100 position-relative">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-20">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-7 d-none d-md-block">
+              <img src="./img/fondo_01.jpg" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height: 100%; width: 140%">
+            </div>
+            
+            <div class="col-md-6 col-lg-5 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
 
-    <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full
-    text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password"
-    id="password" name="password">
-    
-    @error('message')        
-      <p class="border border-red-500 rounded-md bg-red-100 w-full
-      text-red-600 p-2 my-2">* {{ $message }}</p>
-    @enderror
+                <form>
 
-    <div class="flex items-center justify-end mt-4">
-      <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-          {{ __('Forgot your password?') }}
-      </a>
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3" style="color: blue;"></i>
+                    <img src="./img/logo.png" alt="" >
+                  </div>
 
-    <button type="submit" class="rounded-md bg-indigo-500 w-full text-lg
-    text-white font-semibold p-2 my-3 hover:bg-indigo-600">Send</button>
+                  <h5 class="fw-bold mb-4 pb-4" style="letter-spacing: 1.5px;">Iniciar sesión</h5>
+
+                  <!-- Email input -->
+
+                  <div class="form-outline mb-4 flex-nowrap">
+                    <label class="form-label"">Rut</label>
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <input type="email" id="email" name="email" class="form-control form-control-lg border-0" 
+                    placeholder="Ingrese su rut sin puntos ni guión"/>
+                  </div>
+
+                  <! -- Password input -->
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label" for="password">Contraseña</label>
+                    <input type="contrasena" id="password" name="password" class="form-control form-control-lg border-0"
+                    placeholder="Ingrese su contraseña" />
+                  </div>
+
+                  <div class="d-grip gap-2">
+                    <button class="btn btn-primary " type="button" style="border-radius: 1rem; width: 100%">Entrar</button>
+                  </div>
+
+                  @error('message')        
+                    <p class="border border-red-500 rounded-md bg-red-100 w-full
+                    text-red-600 p-2 my-2">* {{ $message }}</p>
+                  @enderror
+
+                  <div class="flex items-center justify-end mt-4">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Recuperar contraseña') }}
+                    </a>
+
+                  
+                  <a class="link-dark" href="#!">Cambiar contraseña</a>
+                     
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
   </form>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,9 +6,9 @@
 
     <title>@yield('title') - Laravel App</title>
     
-    <!-- Tailwind CSS Link -->
-    <link rel="stylesheet" 
-    href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
   </head>
   <body class="bg-gray-100 text-gray-800">
 
