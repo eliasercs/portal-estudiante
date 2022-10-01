162e1df1d248601db2929457b176741c9cb070ff
Hexzzard <hexzzard@gmail.com>
2022-10-01 00:12:35 -0300

    a
    a

--- app/Http/Controllers/RamosController.php
+++ app/Http/Controllers/RamosController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\ramos;
 use Illuminate\Http\Request;
+use DB;
 
 class RamosController extends Controller
 {
-    
     public function index()
     {
         //pagina de inicio 
-        $ramos = ramos::orderBy('id', 'desc')->paginate(3);
-        return view('auth.ramos', compact('ramos'));
+        $ramos = ramos::orderBy('id', 'desc')->paginate(10);
+        return view('auth.inscripcion', compact('ramos'));
     }
 
-
+    public function store($curso){
+        $usuario = auth()->user()->rut;
+        DB::table('inscripcion')->insert([
+            ['rut' => $usuario, 'curso' => $curso,'created_at'=>getdate(),'updated_at'=>getdate()]
+        ]);
+        return "xd";
+    }
 }
