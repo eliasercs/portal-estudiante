3a718f49259a0b9647e99adb07a678c121b62e17
Alvaro Valenzuela <108038544+MelonCaja@users.noreply.github.com>
2022-10-05 16:35:12 -0300

    view-info
    Muestra la info académica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,20 @@
 
 @if(auth()->check())
 <h1 class="text-5xl text-center pt-24">Bienvenido {{ auth()->user()->name }}</h1>
+<div class="text-5xl text-center pt-16">
+  <p>Información academica</p>
+
+  <div class="col-lg-7 text-align-center"">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <h3 class="text-center">Su carrera es: {{ auth()->user()->carrera }}</h3>
+      <h3 class="text-center">Numero de registro: {{ auth()->user()->id }}</h3>
+      <h3 class="text-center">Plan: {{ auth()->user()->plan }}</h3>
+      <h3 class="text-center">Ingreso: {{ auth()->user()->ingreso }}</h3>
+      <h3 class="text-center">Situacion: {{ auth()->user()->situacion }}</h3>
+    </div>
+  </div>
+  
+</div>
 @else
 <h1 class="text-5xl text-center pt-24">Portal del estudiante</h1>
 @endif
