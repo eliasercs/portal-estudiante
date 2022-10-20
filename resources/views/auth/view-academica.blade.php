9da1c8032f775b124da176610f58438f8ad9e65f
Alvaro <avalenzuela_p@outlook.com>
2022-10-19 23:03:57 -0300

    Navbar para todas las paginas

--- resources/views/auth/view-academica.blade.php
+++ resources/views/auth/view-academica.blade.php
@@ -4,7 +4,28 @@
 
 @section('content')
 
-<a href="./home">Home</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/home">UCT</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/Academico">Informacion Academica</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/carreras/new">Registrar Carrera</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/estudiante/matricular">Matricular Estudiante</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/notas">Ver Notas</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 
     rounded-lg shadow-lg">
@@ -34,16 +55,16 @@
             <h5 class="card-title text-center">Estado</h5>
             <hr>
             <p class="card-text">
-                <div class="table table-responsive">
-                    <div class="col-lg-7 text-align-center">
-                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <h3 class="text-center">Carrera: {{ auth()->user()->AcademicRecord->carrera->nombre }}</h3>
+            <div class="col-lg-7 text-align-center">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <h3 class="text-center">Su carrera es:  </h3>
                     <h3 class="text-center">Numero de registro: {{ auth()->user()->id }}</h3>
                     <h3 class="text-center">Plan: {{ auth()->user()->AcademicRecord->plan }}</h3>
                     <h3 class="text-center">Ingreso: {{ auth()->user()->AcademicRecord->ingreso }}</h3>
                     <h3 class="text-center">Situacion: {{ auth()->user()->AcademicRecord->situacion }}</h3>
-                    </div>
+                    <h3 class="text-center">Situacion: {{ auth()->user()->AcademicRecord->CursoInscrito}}</h3>
                 </div>
+            </div>
             </p>
         </div>
         <div class="px-lg-5 py-lg-4 p-4 w-100 align-self-center">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,34 @@
 @section('content')
 
 @if(auth()->check())
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/home">UCT</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/Academico">Informacion Academica</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/carreras/new">Registrar Carrera</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/estudiante/matricular">Matricular Estudiante</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/notas">Ver Notas</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <h1 class="text-5xl text-center pt-24">Bienvenido {{ auth()->user()->name }}</h1>
 <div class="text-5xl text-center pt-16">
   @if(auth()->user()->AcademicRecord)
-  <p>Información academica</p>
-  
-  <div class="col-lg-7 text-align-center">
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-      <h3 class="text-center">Su carrera es: </h3>
-      <h3 class="text-center">Numero de registro: {{ auth()->user()->id }}</h3>
-      <h3 class="text-center">Plan: {{ auth()->user()->AcademicRecord->plan }}</h3>
-      <h3 class="text-center">Ingreso: {{ auth()->user()->AcademicRecord->ingreso }}</h3>
-      <h3 class="text-center">Situacion: {{ auth()->user()->AcademicRecord->situacion }}</h3>
-      <h3 class="text-center">Situacion: {{ auth()->user()->AcademicRecord->CursoInscrito}}</h3>
-    </div>
-  </div>
+
   @endif
   
 </div>
