57ab8eff6fda8d82f07edfc5c80348cbcdd07b23
Alvaro Valenzuela <108038544+MelonCaja@users.noreply.github.com>
2022-11-11 11:56:09 -0300

    Vista Home

---
+++ resources/views/auth/view-home.blade.php
@@ -0,0 +1,59 @@
+@extends('home')
+
+@section('content')
+
+<script defer src="{{ asset('/js/calendario.js') }}"></script>
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="/img/banners_01.png" class="d-block w-100" alt="banner1">
+      </div>
+      <div class="carousel-item">
+        <img src="/img/banners_02.png" class="d-block w-100" alt="banner2">
+      </div>
+      <div class="carousel-item">
+        <img src="/img/slider_01.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+<div class="calendario-pos">
+  <h1 class="title">Calendario Academico</h1>
+
+  <div class="calendar">
+      <div class="calendar__info">
+          <div class="calendar__prev" id="prev-month">&#9664;</div>
+          <div class="calendar__month" id="month">Enero</div>
+          <div class="calendar__year" id="year">2022</div>
+          <div class="calendar__next" id="next-month">&#9654;</div>
+      </div>
+
+      <div class="calendar__week">
+          <div class="calendar__day calendar__item">Lun</div>
+          <div class="calendar__day calendar__item">Mar</div>
+          <div class="calendar__day calendar__item">Mie</div>
+          <div class="calendar__day calendar__item">Jue</div>
+          <div class="calendar__day calendar__item">Vie</div>
+          <div class="calendar__day calendar__item">Sab</div>
+          <div class="calendar__day calendar__item">Dom</div>
+      </div>
+
+      <div class="calendar__dates" id="dates"></div>
+  </div>
+</div>
+
+
+
+  
+  
+@endsection
