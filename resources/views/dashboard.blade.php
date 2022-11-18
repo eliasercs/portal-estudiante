d89a3d9e804ffccf32a2626c328733cd1f89ec6d
Jean Pacheco <paulcitronico@gmail.com>
2022-11-18 12:11:22 -0300

    mejora en el stikers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,32 +4,32 @@
 
 <!-- stikers -->
 <div id="container_stikers">
-    <ul class="stikers">
-      <li class="stikers_li">
-        <a href="#">
-          <h2>Title #1</h2>
-          <p>Text Content #1</p>
-        </a>
-      </li>
-      <li class="stikers_li">
-        <a href="#">
-          <h2>Title #2</h2>
-          <p>Text Content #2</p>
-        </a>
-      </li>
-      <li class="stikers_li">
-        <a href="#">
-          <h2>Title #3</h2>
-          <p>Text Content #3</p>
-        </a>
-      </li>
-      <li class="stikers_li">
-        <a href="#">
-          <h2>Title #4</h2>
-          <p>Text Content #4</p>
-        </a>
-      </li>
-    </ul>
-  </div>
+  <ul class="stikers">
+    <li class="stikers_li">
+      <a href="#">
+        <h2 class="stikers_title">Article #1</h2>
+        <p class="stikers_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fuga suscipit at atque optio aperiam, fugiat nobis.</p>
+      </a>
+    </li>
+    <li class="stikers_li">
+      <a href="#">
+        <h2 class="stikers_title">Article #2</h2>
+        <p class="stikers_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fuga suscipit at atque optio aperiam, fugiat nobis.</p>
+      </a>
+    </li>
+    <li class="stikers_li">
+      <a href="#">
+        <h2 class="stikers_title">Article #3</h2>
+        <p class="stikers_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fuga suscipit at atque optio aperiam, fugiat nobis.</p>
+      </a>
+    </li>
+    <li class="stikers_li">
+      <a href="#">
+        <h2 class="stikers_title">Article #4</h2>
+        <p class="stikers_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fuga suscipit at atque optio aperiam, fugiat nobis.</p>
+      </a>
+    </li>
+  </ul>
+</div>
 
 @endsection
